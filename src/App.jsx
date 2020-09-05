9040c0bedf9824568fb932cb0d10c2f5d4af568e
Javier <javieromar.pxl@gmail.com>
2020-09-05 02:05:24 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header
+        titulo='Cotizador de seguros'
+      />
+    </div>
+  );
+}
+
+export default App;
